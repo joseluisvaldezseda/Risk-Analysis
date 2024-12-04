31f7c837e30eb4749b3f596298647d91153cdeb7
JoseLuis335 <88217428+JoseLuis335@users.noreply.github.com>
2024-12-04 10:08:04 -0600

    Update grafica.py

--- grafica.py
+++ grafica.py
@@ -191,6 +191,8 @@ with col_filtros:
         st.empty()
         st.write("                ")
         st.write("                ")
+        st.write("                ")
+        st.write("                ")
 
         hojas_seleccionadas_disp = st.multiselect("Selecciona las hojas:", list(dfs.keys()), default=["TOTAL CARTERA_resumen"])
         negocios_disp = st.multiselect("Selecciona los negocios:", options=list(colores_negocios_variantes.keys()))
