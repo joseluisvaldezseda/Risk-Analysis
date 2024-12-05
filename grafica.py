0d57acf719eb6783c18da64cdff54bc0f71aecbe
JoseLuis335 <88217428+JoseLuis335@users.noreply.github.com>
2024-12-05 13:44:49 -0600

    Update grafica.py

--- grafica.py
+++ grafica.py
@@ -55,8 +55,7 @@ def crear_grafico_dispersión_multiple(hojas_seleccionadas, negocios_seleccionad
         # Verificar si "MARGEN" existe en el DataFrame
         # Asegurarte de que "MARGEN" exista
         
-        df_filtrado['MARGEN'] = 0  # O usa un valor predeterminado como 0
-        
+        df_filtrado['MARGEN'] = df_filtrado['MARGEN'].fillna(0)        
         # Agrupación y cálculo
         df_filtrado = (
             df_filtrado.groupby(columnas_agrupacion)
