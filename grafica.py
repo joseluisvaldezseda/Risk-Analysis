96927c8a9b1244115bc4eee710341011b42b6c4e
JoseLuis335 <88217428+JoseLuis335@users.noreply.github.com>
2024-12-06 09:22:59 -0600

    Update grafica.py

--- grafica.py
+++ grafica.py
@@ -109,8 +109,8 @@ def crear_grafico_dispersión_multiple(hojas_seleccionadas, negocios_seleccionad
                 text=df_filtrado_var["DEPARTAMENTO / PRODUCTO"],
                 name=f"{negocio} - {eje_y}",  # Nombre del eje Y actual incluyendo el negocio
                 textfont=dict(size=6),
-                textposition='middle right',
-                showlegend=True  # Mantener la entrada en la leyenda
+                textposition='middle right'
+                #showlegend=True  # Mantener la entrada en la leyenda
 
             ))
            # Crear un trace adicional con tamaño fijo SOLO para la leyenda
